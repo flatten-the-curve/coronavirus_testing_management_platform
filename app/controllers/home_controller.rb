ba7a708945b689c2fb0238bad60c875fb2aaad58
1BrandonCummings <brandoncummingsfl@gmail.com>
2020-03-16 19:41:07 -0400

    ggpush

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,13 @@ class HomeController < ApplicationController
   def index
     @user = current_user
     @host = current_user&.host
-    @current_host_marker = [{
-      "lat": @host.latitude,
-      "lng": @host.longitude,
-      "infowindow": "<div>#{@host.name}<br><a target='_blank' href='#{@host.google_maps_url}'>Route me here!</a></div>"
-    }].to_json.html_safe
+    if @host
+      @current_host_marker = [{
+        "lat": @host.latitude,
+        "lng": @host.longitude,
+        "infowindow": "<div>#{@host.name}<br><a target='_blank' href='#{@host.google_maps_url}'>Route me here!</a></div>"
+      }].to_json.html_safe
+    end
     @all_markers = []
     Host.all.each do |h|
       @all_markers.push(
