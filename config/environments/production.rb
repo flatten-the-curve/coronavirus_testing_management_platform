de0dfc81221507e392c65588d534b528d13a422e
1BrandonCummings <brandoncummingsfl@gmail.com>
2020-03-17 08:15:58 -0400

    adding subdomain to mailer of www

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: ENV["RAILS_APP_ROOT_DOMAIN"], port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV["RAILS_APP_ROOT_DOMAIN"], port: 3000, protocol: 'https', subdomain: "www" }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
