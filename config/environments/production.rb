f4d606627ac60958b7a064fe034593aa4c21f969
1BrandonCummings <brandoncummingsfl@gmail.com>
2020-03-17 07:35:02 -0400

    forcing ssl, adding default host name for dev and prod for reset password email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: ENV["RAILS_APP_ROOT_DOMAIN"], port: 3000 }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -47,7 +47,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
