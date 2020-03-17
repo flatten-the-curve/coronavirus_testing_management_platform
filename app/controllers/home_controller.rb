c0beb228e3fbf7fec1ef6c46c0122185f5148454
1BrandonCummings <brandoncummingsfl@gmail.com>
2020-03-17 10:32:38 -0400

    adding current line count to marker info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,22 +3,33 @@ class HomeController < ApplicationController
     @user = current_user
     @host = current_user&.host
     if @host
-      @current_host_marker = [{
-        "lat": @host.latitude,
-        "lng": @host.longitude,
-        "infowindow": "<div>#{@host.name}<br><a target='_blank' href='#{@host.google_maps_url}'>Route me here!</a></div>"
-      }].to_json.html_safe
-      @patient_counts = @host.patient_count_today
-      @line_counts = @host.line_count_today
+      set_host_info
     end
+    set_all_markers
+  end
+
+  private
+
+  def set_host_info
+    @current_host_marker = [{
+      "lat": @host.latitude,
+      "lng": @host.longitude,
+      "infowindow": "<div>#{@host.name}<br><a target='_blank' href='#{@host.google_maps_url}'>Route me here!</a></div>"
+    }].to_json.html_safe
+    @patient_counts = @host.patient_count_today
+    @line_counts = @host.line_count_today
+  end
+
+  def set_all_markers
     @all_markers = []
     Host.all.each do |h|
       @all_markers.push(
         {
           "lat": h.latitude,
           "lng": h.longitude,
-          "infowindow": "<div>#{h.name}<br><a target='_blank' href='#{h.google_maps_url}'>Route me here!</a></div>"
-        }
+          "infowindow": "<div>#{h.name}<br><b>Currently In Line: #{h.line_count_today}</b><br><a target='_blank' href='#{h.google_maps_url}'>Route me here!</a></div>",
+          "icon": 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=1|FE6256|000000',
+      }
       )
     end
     @all_markers = @all_markers.to_json.html_safe
